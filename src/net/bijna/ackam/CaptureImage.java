267aae18dc5b26ca52988caf4ea846e3907ccd45
Kipling Inscore <k@bijna.net>
2011-12-16 10:36:52 -0800

    automatically catpure and store image

--- src/net/bijna/ackam/CaptureImage.java
+++ src/net/bijna/ackam/CaptureImage.java
@@ -1,15 +1,104 @@
 package net.bijna.ackam;
 
 import android.app.Activity;
+import android.content.ContentResolver;
+import android.content.ContentValues;
 import android.os.Bundle;
+import android.hardware.Camera;
+import android.net.Uri;
+import android.provider.MediaStore;
+import android.util.Log;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
+import android.widget.ImageView;
+
+import java.io.IOException;
+import java.io.OutputStream;
 
 public class CaptureImage extends Activity
+	implements SurfaceHolder.Callback,
+	           Camera.PictureCallback
 {
-    /** Called when the activity is first created. */
-    @Override
-    public void onCreate(Bundle savedInstanceState)
-    {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
-    }
+	private static final String TAG = "ackam";
+
+	private Camera camera;
+
+	@Override
+	public void onCreate(Bundle savedInstanceState)
+	{
+		Log.d(TAG, "onCreate()");
+
+		super.onCreate(savedInstanceState);
+
+		final SurfaceView view = new SurfaceView(this);
+		setContentView(view);
+
+		final SurfaceHolder holder = view.getHolder();
+
+		holder.addCallback(this);
+		holder.setType(SurfaceHolder.SURFACE_TYPE_PUSH_BUFFERS);
+	}
+
+	@Override
+	public void surfaceCreated(SurfaceHolder holder)
+	{
+		Log.d(TAG, "surfaceCreated()");
+
+		camera = Camera.open();
+
+		try {
+			camera.setPreviewDisplay(holder);
+		} catch (IOException ex) {
+			Log.e(TAG, "Failed to set camera preview: " + ex.getMessage());
+
+			camera.release();
+			camera = null;
+			finish();
+		}
+	}
+
+	@Override
+	public void surfaceChanged(SurfaceHolder holder, int format,
+	                           int width, int height)
+	{
+		Log.d(TAG, "surfaceChanged()");
+
+		camera.startPreview();
+		camera.takePicture(null, null, this);
+	}
+
+	@Override
+	public void onPictureTaken(byte[] data, Camera camera)
+	{
+		Log.d(TAG, "onPictureTaken()");
+
+		final ContentResolver contentResolver = getContentResolver();
+
+		final Uri uri = contentResolver.insert(
+			MediaStore.Images.Media.EXTERNAL_CONTENT_URI,
+			new ContentValues());
+
+		try {
+			final OutputStream store =
+				contentResolver.openOutputStream(uri);
+
+			store.write(data);
+			store.flush();
+			store.close();
+		} catch (IOException ex) {
+			Log.e(TAG, "Failed to store image: " + ex.getMessage());
+		}
+
+		finish();
+	}
+
+	@Override
+	public void surfaceDestroyed(SurfaceHolder holder)
+	{
+		Log.d(TAG, "surfaceDestroyed()");
+
+		camera.stopPreview();
+		camera.release();
+		camera = null;
+	}
 }
